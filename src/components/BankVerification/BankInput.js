06d717628f5cb72186a08c65c975b30fb986702b
Aravind Srivatsan <aravindsrivats@gmail.com>
2022-01-20 23:48:26 +0530

    Add radio button to choose bank verification type

--- src/components/BankVerification/BankInput.js
+++ src/components/BankVerification/BankInput.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 import { useBank } from '~/src/store/bank'
 
-import { Tip, Label, TextField } from '~/src/styled/Input'
+import { Tip, Label, TextField, Option, RadioButton, SubLabel } from '~/src/styled/Input'
 
 const BankInput = () => {
   const [{ type, ifsc, accountNumber }, setBank] = useBank()
@@ -15,10 +15,31 @@ const BankInput = () => {
   return (
     <>
       <Tip>For bank account verification</Tip>
-      <Label htmlFor='pan'>Enter IFSC</Label>
-      <TextField type='text' name='ifsc' value={ifsc} onChange={input}  />
-      <Label htmlFor='pan'>Enter bank account number</Label>
-      <TextField type='text' name='accountNumber' value={accountNumber} onChange={input}  />
+      <Label>Choose a method</Label>
+      <Option>
+        <RadioButton type='radio' value='upi' name='type' checked={type === 'upi'} onChange={input} />
+        <Label>
+          Verify with UPI
+          <SubLabel>Complete a test payment on any UPI app - PhonePe, Google Pay etc.</SubLabel>
+        </Label>
+      </Option>
+      <Option>
+        <RadioButton type='radio' value='bank' name='type' checked={type === 'bank'} onChange={input} />
+        <Label>
+          Verify with bank account information
+          <SubLabel>Provide your bank account number and IFSC code</SubLabel>
+        </Label>
+      </Option>
+      {
+        type === 'bank' ? (
+          <>
+          <Label htmlFor='ifsc'>Enter IFSC</Label>
+          <TextField type='text' name='ifsc' value={ifsc} onChange={input}  />
+          <Label htmlFor='accountNumber'>Enter bank account number</Label>
+          <TextField type='text' name='accountNumber' value={accountNumber} onChange={input}  />
+          </>
+        ) : null
+      }
     </>
   )
 }
--- src/styled/Input.js
+++ src/styled/Input.js
@@ -24,4 +24,51 @@ padding: 15px;
 margin-bottom: 40px;
 `
 
-export { Tip, Label, TextField }
+const Option = styled.div`
+  display: flex;
+  width: calc(100% - 30px);
+  padding: 15px;
+  background: #FFFFFF;
+  border: thin solid #D8D8D8;
+  border-radius: 5px;
+  margin-bottom: 25px;
+`
+
+const RadioButton = styled.input`
+  box-sizing: border-box;
+  appearance: none;
+  margin: 0;
+  margin-right: 15px;
+  width: 2.5rem;
+  height: 1.3rem;
+  background: #E2E6E9;
+  border: thin solid #53B08F;
+  border-radius: 50%;
+  display: grid;
+  place-content: center;
+  &:before {
+    content: '';
+    width: 0.7em;
+    height: 0.6em;
+    border-radius: 50%;
+    transform: scale(0);
+    transition: 120ms transform ease;
+    box-shadow: inset 1em 1em #FFFFFF;
+  }
+  &:checked {
+    background: #53B08F;
+    &:before { 
+      transform: scale(1);
+    }
+  }
+`
+
+const SubLabel = styled.span`
+  display: inline-block;
+  font-weight: 400;
+  font-size: 0.8rem;
+  margin-top: 15px;
+  color: #8D8D8D;
+`
+
+export { Tip, Label, TextField, Option, RadioButton, SubLabel }
